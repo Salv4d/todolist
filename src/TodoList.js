e27e371affede1db5ef778a615181d745381d2e1
Salvador Oliveira <slsalvadorlucas@gmail.com>
2022-02-15 19:52:55 -0400

    add uuidv4 and fix remoteTodo bind

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,16 +1,26 @@
 import { Component } from "react";
 import Todo from "./Todo";
 import NewTodoForm from "./NewTodoForm";
+import { v4 as uuidv4 } from "uuid";
 
 class TodoList extends Component {
   constructor(props) {
     super(props);
     this.state = { todos: new Array() };
     this.addTodo = this.addTodo.bind(this);
+    this.removeTodo = this.removeTodo.bind(this);
   }
 
   addTodo(newTodo) {
-    this.setState((st) => ({ todos: [...st.todos, newTodo] }));
+    this.setState((st) => ({
+      todos: [...st.todos, { ...newTodo, id: uuidv4() }],
+    }));
+  }
+
+  removeTodo(todoId) {
+    this.setState((st) => ({
+      todos: st.todos.filter((task) => task.id != todoId),
+    }));
   }
 
   render() {
@@ -18,7 +28,12 @@ class TodoList extends Component {
       <div className="TodoList">
         <NewTodoForm addTodo={this.addTodo} />
         {this.state.todos.map((task) => (
-          <Todo task={task.name} key={task.id} />
+          <Todo
+            task={task.name}
+            key={task.id}
+            id={task.id}
+            removeTodo={this.removeTodo}
+          />
         ))}
       </div>
     );
