0b8b3577a83a48c70f2a88af9373c71ec3c216b0
Salvador Oliveira <slsalvadorlucas@gmail.com>
2022-02-15 17:59:23 -0400

    add submit and change handler for newtodoform component

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -1,11 +1,34 @@
 import { Component } from "react";
 
 class NewTodoForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { todo: "" };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit() {
+    this.props.addTodo(this.state.todo);
+
+    this.state = {
+      todo: "",
+    };
+  }
+
+  handleChange(evt) {
+    this.setState(() => ({ todo: evt.target.value }));
+  }
+
   render() {
     return (
       <div className="NewTodoForm">
-        <form>
-          <input type="text" />
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.todo}
+            onChange={this.handleChange}
+          />
           <button>Add todo</button>
         </form>
       </div>
